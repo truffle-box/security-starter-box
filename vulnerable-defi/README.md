0ffcd6b39dd6ba43e4cedc3ea254741184e99be5
MB <951378+michaeljohnbennett@users.noreply.github.com>
2022-09-26 16:20:15 +1000

    Readme and package json fies.

---
+++ vulnerable-defi/README.md
@@ -0,0 +1,9 @@
+# Damn Vulnerable DeFi - Truffle Box
+
+This repo has some nice examples of defi contracts with vulnerabilities with different tests and examples of mitigation.
+
+## Unstoppable Deployment
+
+We can deploy the unstoppable contracts now with yarn commands.
+
+Don't forget to disarm the contracts after deplopying if you want to run them against a fuzzing service tool if it needs them armed with scribble annotations.
--- vulnerable-defi/truffle-config.js
+++ vulnerable-defi/truffle-config.js
@@ -41,11 +41,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -79,6 +79,10 @@ module.exports = {
   // Set default mocha options here, use special reporters, etc.
   mocha: {
     timeout: 20000
+  },
+  // needing enabled for fuzzing client compatibility.
+  db: {
+    enabled: true
   },
 
   // Configure your compilers
